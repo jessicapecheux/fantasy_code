2fbb4e6b7c2a3fc74ccb9503ea71113ef249a4c4
Yoann Brambilla <yobrambilla@gmail.com>
2022-03-18 14:14:58 +0100

    changements

--- _ourteam.php
+++ _ourteam.php
@@ -1,110 +1,101 @@
-<?php include("index.php"); ?> 
+<?php include("index.php"); ?>
 
-    <h1>Our Team</h1>
-    <section class ="ourteam">
-      <h2>Hulk Hogan</h2>
-      <div class="Hulk">
-        <img
-          src="/assets/images/hulk.png"
-          alt="Hulk"
-          width="140rem"
-          class="hulkPic"
-        />
-        <p class="hulkQuote">
-          <i>
-            " ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium? "</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Facilis quod ipsam voluptatum officiis,
-          nostrum minima alias pariatur tenetur sit soluta corporis aut eveniet
-          tempore dolor a dolore dignissimos laudantium odit?
-        </p>
-      </div>
-      <h2>Homer Simpson</h2>
-      <div class="Homer">
-        <img
-          src="/assets/images/homer.png"
-          alt="Hulk"
-          width="140rem"
-          class="homerPic"
-        />
-        <p class="homerQuote">
-          <i>
-            ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium?</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Sint ipsa ex deserunt eos, ipsam et quo.
-          Cumque saepe voluptatum illo repellendus, impedit itaque ducimus
-          incidunt nam eligendi eum? Nihil, placeat!
-        </p>
-      </div>
-      <h2><strong>Batman </strong><i> Alias </i> "Bruce Wayne"</h2>
-      <div class="Batman">
-        <img
-          src="/assets/images/batman.png"
-          alt="Hulk"
-          width="200rem"
-          class="batmanPic"
-        />
-        <p class="batmanQuote">
-          <i>
-            ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium?</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Nemo, sint illo. Placeat similique ipsa illum, est
-          quo odit voluptas delectus odio, voluptatibus eaque voluptate expedita
-          hic repudiandae earum! Doloremque, impedit?
-        </p>
-      </div>
-      <h2>Master Yoda</h2>
-      <div class="Yoda">
-        <img
-          src="/assets/images/yoda.png"
-          alt="Hulk"
-          width="200rem"
-          class="yodaPic"
-        />
-        <p class="yodaQuote">
-          <i>
-            ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium?</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Iusto nulla sint harum eum accusamus dignissimos,
-          odio unde dolores inventore mollitia magni, amet ipsum ad. Quae earum
-          quaerat eveniet aliquid rem.
-        </p>
-      </div>
-    </section>
- 
+<h1>Our Team</h1>
+<section class="ourTeam">
+  <h2>" Hulk Hogan "</h2>
+  
+  <div class="hulkPic">
+    <img src="/assets/images/hulk.png" alt="Hulk" width="140rem" />
+    </div>
+  <div class="hulkQuote">
+    <p>
+      <i>
+        " ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium? "</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor, sit amet
+    consectetur adipisicing elit. Facilis quod ipsam voluptatum officiis,
+    nostrum minima alias pariatur tenetur sit soluta corporis aut eveniet
+    tempore dolor a dolore dignissimos laudantium odit?
+  </p>
+  
+  <h2>" Homer Simpson "</h2>
+  <div class="homerPic">
+    <img src="/assets/images/homer.png" alt="Hulk" width="140rem" />
+  </div>
+  <div class="homerQuote">
+    <p>
+
+
+
+      <i>
+        ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium?</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Sint ipsa ex deserunt eos, ipsam et quo.
+    Cumque saepe voluptatum illo repellendus, impedit itaque ducimus
+    incidunt nam eligendi eum? Nihil, placeat!
+  </p>
+
+  <h2><strong>Batman </strong><i> Alias </i>  " Bruce Wayne "</h2>
+  <div class="batmanPic">
+    <img src="/assets/images/batman.png" alt="Hulk" width="200rem" />
+  </div>
+  <div class="batmanQuote">
+    <p>
+      <i>
+        ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium?</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Nemo, sint illo. Placeat similique ipsa illum, est
+    quo odit voluptas delectus odio, voluptatibus eaque voluptate expedita
+    hic repudiandae earum! Doloremque, impedit?
+  </p>
+
+  <h2>" Master Yoda "</h2>
+  <div class="yodaPic">
+    <img src="/assets/images/yoda.png" alt="Hulk" width="200rem" />
+  </div>
+  <div class="yodaQuote">
+    <p>
+      <i>
+        ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium?</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Iusto nulla sint harum eum accusamus dignissimos,
+    odio unde dolores inventore mollitia magni, amet ipsum ad. Quae earum
+    quaerat eveniet aliquid rem.
+  </p>
+
+</section>
