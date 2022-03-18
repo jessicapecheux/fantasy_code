0a94330eb9c1e415568ba5b54ded18454fe41f38
J007B <c.fougeroux007@gmail.com>
2022-03-18 10:16:10 +0100

    add icon_media

--- _footer.php
+++ _footer.php
@@ -1,6 +1,13 @@
-<footer>    
-        <div class="langage"><a href="#">Front-end</a></div>
-        <div class="langage"><a href="#">Back-end</a></div>
-        <div class="langage"><a href="#">Designer</a></div>
-        <div class="langage"><a href="#">Data</a></div>
+<footer class= "contact_and_media">
+        <div id= "contact_us"><p>Contact us</p></div> 
+        <div id= "icon_medias">
+                <div id="icon_facebook"><img src="assets/images/facebook.png" alt="facebook"  /></div>
+                <div id="icon_twitter"><img src="assets/images/twitter.png" alt="twitter"  /></div>
+                <div id="icon_instagram"><img src="assets/images/instagram.png" alt="instagram"  /></div>
+                <div id="icon_gmail"><img src="assets/images/gmail.png" alt="gmail"  /></div>
+                <div id="icon_phone"><img src="assets/images/phone.png" alt="phone"  /></a></div>
+        </div>
+                <div id= "copyright"><p>copyright</p></div>
+        
+        
 </footer>
