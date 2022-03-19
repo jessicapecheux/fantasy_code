e4dc5b97afd5e5af29c586474e30059a6327f3c9
Yoann Brambilla <yobrambilla@gmail.com>
2022-03-19 06:07:13 +0100

    work continue and work on desktop version

--- _ourteam.php
+++ _ourteam.php
@@ -1,110 +1,84 @@
-<?php include("index.php"); ?>
+<?php include("_header.php"); ?>
+<script src="assets/js/script.js"></script>
+<link rel="stylesheet" href="/assets/css/_ourteam.css">
+<h1>Our Team</h1>
+<section class="ourteam">
+  <h2>" Hulk Hogan "</h2>
 
-    <h1>Our Team</h1>
-    <section class ="ourteam">
-      <h2>Hulk Hogan</h2>
-      <div class="Hulk">
-        <img
-          src="/assets/images/hulk.png"
-          alt="Hulk"
-          width="140rem"
-          class="hulkPic"
-        />
-        <p class="hulkQuote">
-          <i>
-            " ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium? "</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Facilis quod ipsam voluptatum officiis,
-          nostrum minima alias pariatur tenetur sit soluta corporis aut eveniet
-          tempore dolor a dolore dignissimos laudantium odit?
-        </p>
-      </div>
-      <h2>Homer Simpson</h2>
-      <div class="Homer">
-        <img
-          src="/assets/images/homer.png"
-          alt="Hulk"
-          width="140rem"
-          class="homerPic"
-        />
-        <p class="homerQuote">
-          <i>
-            ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium?</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Sint ipsa ex deserunt eos, ipsam et quo.
-          Cumque saepe voluptatum illo repellendus, impedit itaque ducimus
-          incidunt nam eligendi eum? Nihil, placeat!
-        </p>
-      </div>
-      <h2><strong>Batman </strong><i> Alias </i> "Bruce Wayne"</h2>
-      <div class="Batman">
-        <img
-          src="/assets/images/batman.png"
-          alt="Hulk"
-          width="200rem"
-          class="batmanPic"
-        />
-        <p class="batmanQuote">
-          <i>
-            ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium?</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Nemo, sint illo. Placeat similique ipsa illum, est
-          quo odit voluptas delectus odio, voluptatibus eaque voluptate expedita
-          hic repudiandae earum! Doloremque, impedit?
-        </p>
-      </div>
-      <h2>Master Yoda</h2>
-      <div class="Yoda">
-        <img
-          src="/assets/images/yoda.png"
-          alt="Hulk"
-          width="200rem"
-          class="yodaPic"
-        />
-        <p class="yodaQuote">
-          <i>
-            ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
-            a accusantium optio sint laudantium repellat, voluptatum cupiditate
-            perferendis, vel pariatur magnam deleniti tempora! Quia debitis
-            labore incidunt praesentium?</i
-          >
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-          assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-          nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Iusto nulla sint harum eum accusamus dignissimos,
-          odio unde dolores inventore mollitia magni, amet ipsum ad. Quae earum
-          quaerat eveniet aliquid rem.
-        </p>
-      </div>
-    </section>
- 
+  <h3>Front End Dev</h3>
+  <div class="hulk">
+    <img src="/assets/images/hulk.png" alt="Hulk" width="140rem" class="hulkPic" />
+    <p class="hulkQuote">
+      <i>
+        " Cool but angry "</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor, sit amet
+    consectetur adipisicing elit. Facilis quod ipsam voluptatum officiis,
+    nostrum minima alias pariatur tenetur sit soluta corporis aut eveniet
+    tempore dolor a dolore dignissimos laudantium odit?
+  </p>
+
+  <h2>" Homer Simpson "</h2>
+  <h3>Data Analyst</h3>
+  <div class="homer">
+    <img src="/assets/images/homer.png" alt="Homer" width="140rem" class="homerPic" />
+    <p class="homerQuote">
+      <i>
+        " I Am So Smart! I Am So Smart! S-M-R-T! I Mean S-M-A-R-T! "</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Sint ipsa ex deserunt eos, ipsam et quo.
+    Cumque saepe voluptatum illo repellendus, impedit itaque ducimus
+    incidunt nam eligendi eum? Nihil, placeat!
+  </p>
+
+  <h2><strong>Batman </strong><i> Alias </i> " Bruce Wayne "</h2>
+  <h3>Back End Dev</h3>
+  <div class="batman">
+    <img src="/assets/images/batman.png" alt="Batman" width="200rem" class="batmanPic" />
+    <p class="batmanQuote">
+      <i>
+        " It’s Not Who I Am Underneath, But What I Do That Defines Me "</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Nemo, sint illo. Placeat similique ipsa illum, est
+    quo odit voluptas delectus odio, voluptatibus eaque voluptate expedita
+    hic repudiandae earum! Doloremque, impedit?
+  </p>
+
+  <h2>" Master Yoda "</h2>
+  <h3>Web Designer</h3>
+  <div class="yoda">
+    <img src="/assets/images/yoda.png" alt="Yoda" width="250rem" class="yodaPic" />
+    <p class="yodaQuote">
+      <i>
+        " Do or do not. There is no try "</i>
+    </p>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Iusto nulla sint harum eum accusamus dignissimos,
+    odio unde dolores inventore mollitia magni, amet ipsum ad. Quae earum
+    quaerat eveniet aliquid rem.
+  </p>
+
+</section>
+<?php include("_footer.php"); ?>
