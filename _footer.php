a67556449fc1e0642f4628f9978fc70b0a51e512
J007B <c.fougeroux007@gmail.com>
2022-03-20 20:55:11 +0100

    add icons links

--- _footer.php
+++ _footer.php
@@ -1,11 +1,21 @@
 <footer class= "contact_and_media">
         <a href="" id= "contact_us"><p>Contact us</p></a>
         <div class= "icon_medias">
-                <a href=""><img src="assets/images/facebook.png" alt="facebook"  /></a>
-                <a href=""><img src="assets/images/twitter.png" alt="twitter"  /></a>
-                <a href=""><img src="assets/images/instagram.png" alt="instagram"  /></a>
-                <a href=""><img src="assets/images/gmail.png" alt="gmail"  /></a>
-                <a href="tel=0000000007"><img src="assets/images/phone.png" alt="phone"  /></a>
+                <a href="https://fr-fr.facebook.com/">
+                        <img src="assets/images/facebook.png" alt="facebook"  />
+                </a>
+                <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZnIifQ%3D%3D%22%7D">
+                        <img src="assets/images/twitter.png" alt="twitter"  />
+                </a>
+                <a href="https://www.instagram.com/?hl=fr">
+                        <img src="assets/images/instagram.png" alt="instagram"  />
+                </a>
+                <a href="https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
+                        <img src="assets/images/gmail.png" alt="gmail"  />
+                </a>
+                <a href="tel:=+33000000007">
+                        <img src="assets/images/phone.png" alt="phone"  />
+                </a>
         </div>
         <div id= "copyright">copyright</div>
 </footer>
