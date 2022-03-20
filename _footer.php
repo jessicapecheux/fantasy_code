4c2fc606ff9585e097d44f6cff598db62dd843dc
J007B <c.fougeroux007@gmail.com>
2022-03-20 12:38:41 +0100

    change in footer structure

--- _footer.php
+++ _footer.php
@@ -1,15 +1,11 @@
 <footer class= "contact_and_media">
-        
+        <a href="" id= "contact_us"><p>Contact us</p></a>
         <div class= "icon_medias">
-                <a href="" id= "contact_us"><p>Contact us</p></a> 
                 <a href=""><img src="assets/images/facebook.png" alt="facebook"  /></a>
                 <a href=""><img src="assets/images/twitter.png" alt="twitter"  /></a>
                 <a href=""><img src="assets/images/instagram.png" alt="instagram"  /></a>
                 <a href=""><img src="assets/images/gmail.png" alt="gmail"  /></a>
-                <a href=""><img src="assets/images/phone.png" alt="phone"  /></a>
-                <div id= "copyright">copyright</div>
+                <a href="tel=0000000007"><img src="assets/images/phone.png" alt="phone"  /></a>
         </div>
-        
-        
-        
+        <div id= "copyright">copyright</div>
 </footer>
---
+++ ourteam.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Fantasy code</title>
+</head>
+<body>
+<?php include("_header.php"); ?>
+
+<h1>Our Team</h1>
+<section class ="ourteam">
+  <h2>Hulk Hogan</h2>
+  <div class="Hulk">
+    <img
+      src="/assets/images/hulk.png"
+      alt="Hulk"
+      width="140rem"
+      class="hulkPic"
+    />
+    <p class="hulkQuote">
+      <i>
+        " ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium? "</i
+      >
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+      assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+      nisi, nam quidem deleniti!Lorem Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis quod ipsam voluptatum officiis,
+      nostrum minima alias pariatur tenetur sit soluta corporis aut eveniet
+      tempore dolor a dolore dignissimos laudantium odit?
+    </p>
+  </div>
+  <h2>Homer Simpson</h2>
+  <div class="Homer">
+    <img
+      src="/assets/images/homer.png"
+      alt="Hulk"
+      width="140rem"
+      class="homerPic"
+    />
+    <p class="homerQuote">
+      <i>
+        ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium?</i
+      >
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+      assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+      nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Sint ipsa ex deserunt eos, ipsam et quo.
+      Cumque saepe voluptatum illo repellendus, impedit itaque ducimus
+      incidunt nam eligendi eum? Nihil, placeat!
+    </p>
+  </div>
+  <h2><strong>Batman </strong><i> Alias </i> "Bruce Wayne"</h2>
+  <div class="Batman">
+    <img
+      src="/assets/images/batman.png"
+      alt="Hulk"
+      width="200rem"
+      class="batmanPic"
+    />
+    <p class="batmanQuote">
+      <i>
+        ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium?</i
+      >
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+      assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+      nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Nemo, sint illo. Placeat similique ipsa illum, est
+      quo odit voluptas delectus odio, voluptatibus eaque voluptate expedita
+      hic repudiandae earum! Doloremque, impedit?
+    </p>
+  </div>
+  <h2>Master Yoda</h2>
+  <div class="Yoda">
+    <img
+      src="/assets/images/yoda.png"
+      alt="Hulk"
+      width="200rem"
+      class="yodaPic"
+    />
+    <p class="yodaQuote">
+      <i>
+        ipsum dolor sit amet consectetur adipisicing elit. Labore ad officia
+        a accusantium optio sint laudantium repellat, voluptatum cupiditate
+        perferendis, vel pariatur magnam deleniti tempora! Quia debitis
+        labore incidunt praesentium?</i
+      >
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
+      assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
+      nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Iusto nulla sint harum eum accusamus dignissimos,
+      odio unde dolores inventore mollitia magni, amet ipsum ad. Quae earum
+      quaerat eveniet aliquid rem.
+    </p>
+  </div>
+</section>
+<?php include("_footer.php"); ?>
+     <script src="assets/js/script.js"></script>
+</body>
+</html>
