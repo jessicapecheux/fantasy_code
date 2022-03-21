97e58209e60dee4b557e55f4ce6f8ffa9d330c53
Yoann Brambilla <yobrambilla@gmail.com>
2022-03-21 13:42:52 +0100

    Keep working

--- ourteam.php
+++ ourteam.php
@@ -10,9 +10,11 @@
     <img src="/assets/images/hulk.png" alt="Hulk" width="140rem" class="hulkPic" />
     <p class="hulkQuote">
       <i>
-        " Cool but angry "</i>
+        " Cool but angry "
+      </i>
     </p>
   </div>
+
   <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
     qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
@@ -29,7 +31,7 @@
     <img src="/assets/images/homer.png" alt="Homer" width="140rem" class="homerPic" />
     <p class="homerQuote">
       <i>
-        " I Am So Smart! I Am So Smart! S-M-R-T! I Mean S-M-A-R-T! "</i>
+        " I Am So Smart!</br>so S.M.R.T ! d'oh "</i>
     </p>
   </div>
   <p>
@@ -45,10 +47,10 @@
   <h2><strong>Batman </strong><i> Alias </i> " Bruce Wayne "</h2>
   <h3>Back End Dev</h3>
   <div class="batman">
-    <img src="/assets/images/batman.png" alt="Batman" width="200rem" class="batmanPic" />
+    <img src="/assets/images/batman.png" alt="Batman" width="140rem" class=" batmanPic" />
     <p class="batmanQuote">
       <i>
-        " It’s Not Who I Am Underneath, But What I Do That Defines Me "</i>
+        " You really want </br> became a Joker "</i>
     </p>
   </div>
   <p>
@@ -64,10 +66,10 @@
   <h2>" Master Yoda "</h2>
   <h3>Web Designer</h3>
   <div class="yoda">
-    <img src="/assets/images/yoda.png" alt="Yoda" width="250rem" class="yodaPic" />
+    <img src="/assets/images/yoda.png" alt="Yoda" width="240rem" class="yodaPic" />
     <p class="yodaQuote">
       <i>
-        " Do or do not. There is no try "</i>
+        " Do or do not.</br> There is no try "</i>
     </p>
   </div>
   <p>
