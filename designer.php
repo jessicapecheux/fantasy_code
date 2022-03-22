633d40749d44f616ddc0478e6de0effb8b6e0cd6
Yoann Brambilla <yobrambilla@gmail.com>
2022-03-22 06:50:57 +0100

    start work on designer.php

--- designer.php
+++ designer.php
@@ -2,9 +2,22 @@
 <script src="assets/js/script.js"></script>
 <link rel="stylesheet" href="/assets/css/_designer.css">
 
+<h1>Web Designer</h1>
 
+<h2>What is Web Design?</h2>
 
+<p>Web design (page design) is the art and science of building the look, feel, and how a website and web page functions in a nutshell. Having a clear user interface and easy to use website will ultimately lead to a better user experience for your target audience. There are many aspects of successful web design like HTML, HTML5, cascading style sheets, colors, JavaScript, responsive design, layouts, social media, text size, graphics, and so much more. Understanding a plethora of programming languages back-end code development, and more will be critical for a web browser rendering your content. If you've been wondering how to learn web and ux design, you've come to the right place!</p>
 
+<h2>Web Designing Courses for Beginners</h2>
 
+<p>edX provides web design training with free online courses for all levels. Learn web design with introductory tutorials and courses from some of the world's leading universities. The World Wide Web Consortium (W3C)'s CSS Basics teaches you about multiple design topics including best practices in web design, design principles, design tools, and much more. You will learn how to take your Web pages from bland to bold with the power of CSS styling. Learn the basic selection of CSS properties, fundamental CSS selectors, and designing page layout through relative positioning with CSS</p>
+
+<h2>Online Courses in Web Design</h2>
+
+<p>As mentioned above, web and graphic design is an integral part of having a successful web presence. Here at edX, we offer a wide range of web design and ui design classes to suit your needs. We offer some web design courses online for free to help advance your career. Whether you are looking for web designing courses for beginners, or something more advanced, we've got you covered. At edX, we provide only the best online web design courses to help you succeed.</p>
+
+<h2>Jobs in Web Design</h2>
+
+<p>According to Glassdoor, you can expect an average salary of $64,468 in the United States for web design. As your experience grows in website design, you can expect to see higher salary ranges. For example, you can expect an average Junior Web Designer to make around $62k in the United States. As a Front End Web Developer, you can expect to make over $90k. Take a web design class today!</p>
 
 <?php include("_footer.php"); ?>
