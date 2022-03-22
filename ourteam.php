c25e2150a3c403f1d9cdf0dc4a5a9979d5e2e300
Yoann Brambilla <yobrambilla@gmail.com>
2022-03-22 06:28:17 +0100

    Finish responsive and change contact.php

--- ourteam.php
+++ ourteam.php
@@ -3,11 +3,11 @@
 <link rel="stylesheet" href="/assets/css/_ourteam.css">
 <h1>Our Team</h1>
 <section class="ourteam">
-  <h2>" Hulk Hogan "</h2>
+  <h2>" Hulk <i>Aka</i> Bruce Banner "</h2>
 
   <h3>Front End Dev</h3>
   <div class="hulk">
-    <img src="/assets/images/hulk.png" alt="Hulk" width="140rem" class="hulkPic" />
+    <img src="/assets/images/hulk.png" alt="Hulk" width="240rem" class="hulkPic" />
     <p class="hulkQuote">
       <i>
         " Cool but angry "
@@ -16,70 +16,56 @@
   </div>
 
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor, sit amet
-    consectetur adipisicing elit. Facilis quod ipsam voluptatum officiis,
-    nostrum minima alias pariatur tenetur sit soluta corporis aut eveniet
-    tempore dolor a dolore dignissimos laudantium odit?
+    The Hulk is a fictional superhero appearing in publications by the American publisher Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, the character first appeared in the debut issue of The Incredible Hulk (May 1962). In his comic book appearances, the character is both the Hulk, a green-skinned, hulking and muscular humanoid possessing a limitless degree of physical strength, and his alter ego Dr. Robert Bruce Banner, a physically weak, socially withdrawn, and emotionally reserved physicist. The two exist as independent dissociative personalities, and resent each other.</br></br>
+
+    Following his accidental exposure to gamma rays while saving the life of Rick Jones during the detonation of an experimental bomb, Banner is physically transformed into the Hulk when subjected to emotional stress, at or against his will. This transformation often leads to destructive rampages and to conflicts that complicate Banner's civilian life. The Hulk's level of strength is normally conveyed as proportionate to his level of anger. Commonly portrayed as a raging savage, the Hulk has been represented with other personalities based on Banner's fractured psyche, from a mindless, destructive force, to a brilliant warrior, or a genius scientist in his own right. Despite both Hulk and Banner's desire for solitude, the character has a large supporting cast. This includes Banner's lover Betty Ross, his best friend Rick Jones, his cousin She-Hulk, and therapist and ally Doc Samson. In addition, the Hulk alter ego has many key supporting characters, like his co-founders of the superhero team the Avengers, his queen Caiera, fellow warriors Korg and Miek, and sons Skaar and Hiro-Kala. However, his uncontrollable power has brought him into conflict with his fellow heroes and others. Despite this, he tries his best to do what's right while battling villains such as the Leader, the Abomination, the Absorbing Man and more.</br></br>
+
+    Lee stated that the Hulk's creation was inspired by a combination of Frankenstein and Dr. Jekyll and Mr. Hyde. Although the Hulk's coloration has varied throughout the character's publication history, the most usual color is green.
   </p>
 
   <h2>" Homer Simpson "</h2>
   <h3>Data Analyst</h3>
   <div class="homer">
-    <img src="/assets/images/homer.png" alt="Homer" width="140rem" class="homerPic" />
+    <img src="/assets/images/homer.png" alt="Homer" width="200rem" class="homerPic" />
     <p class="homerQuote">
       <i>
         " I Am So Smart!</br>so S.M.R.T ! d'oh "</i>
     </p>
   </div>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet,
-    consectetur adipisicing elit. Sint ipsa ex deserunt eos, ipsam et quo.
-    Cumque saepe voluptatum illo repellendus, impedit itaque ducimus
-    incidunt nam eligendi eum? Nihil, placeat!
+    Homer Jay Simpson is one of the main characters of the American animated sitcom The Simpsons. He is voiced by Dan Castellaneta and first appeared, along with the rest of his family, in The Tracey Ullman Show short "Good Night" on April 19, 1987. Homer was created and designed by cartoonist Matt Groening while he was waiting in the lobby of producer James L. Brooks's office. Groening had been called to pitch a series of shorts based on his comic strip Life in Hell but instead decided to create a new set of characters. He named the character after his father, Homer Groening. After appearing for three seasons on The Tracey Ullman Show, the Simpson family got their own series on Fox, which debuted December 17, 1989. The show was later acquired by Disney in 2019.</br></br>
+
+    As the nominal foreman of the paternally eponymous family, Homer and his wife Marge have three children: Bart, Lisa and Maggie. As the family's provider, he works at the Springfield Nuclear Power Plant as safety inspector. Homer embodies many American working class stereotypes: he is obese, immature, outspoken, aggressive, balding, lazy, ignorant, unprofessional, and addicted to beer, junk food and watching television. However, he is fundamentally a good man and is staunchly protective of his family, especially when they need him the most. Despite the suburban blue-collar routine of his life, he has had a number of remarkable experiences, including going to space, climbing the tallest mountain in Springfield by himself, fighting former President George H. W. Bush and winning a Grammy Award as a member of a barbershop quartet.
   </p>
 
-  <h2><strong>Batman </strong><i> Alias </i> " Bruce Wayne "</h2>
+  <h2><strong>" Batman </strong><i> Aka </i> Bruce Wayne "</h2>
   <h3>Back End Dev</h3>
   <div class="batman">
-    <img src="/assets/images/batman.png" alt="Batman" width="140rem" class=" batmanPic" />
+    <img src="/assets/images/batman.png" alt="Batman" width="340rem" class=" batmanPic" />
     <p class="batmanQuote">
       <i>
         " You really want </br> became a Joker "</i>
     </p>
   </div>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
-    adipisicing elit. Nemo, sint illo. Placeat similique ipsa illum, est
-    quo odit voluptas delectus odio, voluptatibus eaque voluptate expedita
-    hic repudiandae earum! Doloremque, impedit?
+    Batman is a superhero appearing in American comic books published by DC Comics. The character was created by artist Bob Kane and writer Bill Finger, and debuted in the 27th issue of the comic book Detective Comics on March 30, 1939. In the DC Universe continuity, Batman is the alias of Bruce Wayne, a wealthy American playboy, philanthropist, and industrialist who resides in Gotham City. Batman's origin story features him swearing vengeance against criminals after witnessing the murder of his parents Thomas and Martha, a vendetta tempered with the ideal of justice. He trains himself physically and intellectually, crafts a bat-inspired persona, and monitors the Gotham streets at night. Kane, Finger, and other creators accompanied Batman with supporting characters, including his sidekicks Robin and Batgirl; allies Alfred Pennyworth, James Gordon, and Catwoman; and foes such as the Penguin, the Riddler, Two-Face, and his archenemy the Joker.</br></br>
+
+    Kane conceived Batman in early 1939 to capitalize on the popularity of DC's Superman; although Kane frequently claimed sole creation credit, Finger substantially developed the concept from a generic superhero into something more bat-like. The character received his own spin-off publication, Batman, in 1940. Batman was originally introduced as a ruthless vigilante who frequently killed or maimed criminals, but evolved into a character with a stringent moral code and strong sense of justice. Unlike most superheroes, Batman does not possess any superpowers, instead relying on his intellect, fighting skills, and wealth. The 1960s Batman television series used a camp aesthetic, which continued to be associated with the character for years after the show ended. Various creators worked to return the character to his darker roots in the 1970s and 1980s, culminating with the 1986 miniseries The Dark Knight Returns by Frank Miller.
   </p>
 
   <h2>" Master Yoda "</h2>
   <h3>Web Designer</h3>
   <div class="yoda">
-    <img src="/assets/images/yoda.png" alt="Yoda" width="240rem" class="yodaPic" />
+    <img src="/assets/images/yoda.png" alt="Yoda" width="340rem" class="yodaPic" />
     <p class="yodaQuote">
       <i>
         " Do or do not.</br> There is no try "</i>
     </p>
   </div>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-    qui ipsam saepe quasi temporibus ab exercitationem perferendis alias
-    assumenda repellendus accusamus iusto quisquam harum ullam reiciendis
-    nisi, nam quidem deleniti!Lorem Lorem ipsum dolor sit amet consectetur
-    adipisicing elit. Iusto nulla sint harum eum accusamus dignissimos,
-    odio unde dolores inventore mollitia magni, amet ipsum ad. Quae earum
-    quaerat eveniet aliquid rem.
+    Yoda (/ˈjoʊdə/) is a fictional character in the Star Wars universe, first appearing in the 1980 film The Empire Strikes Back. He is a small, green humanoid alien who is powerful with the Force and served as Grandmaster of the Jedi Order. In The Empire Strikes Back, Yoda was voiced and puppeteered by Frank Oz, who reprised the role in Return of the Jedi, the prequel trilogy, and the sequel trilogy. Outside of the films, the character has been mainly voiced by Tom Kane, starting with the 2003 Clone Wars animated television series. Yoda has since become an iconic figure in popular culture due to his distinct pattern of speech and role as a wise mentor.</br></br>
+
+    In his first appearance in the original trilogy, Yoda is described as the mentor of Obi-Wan Kenobi, and lives in exile on the swamp planet of Dagobah. He trains Luke Skywalker in the ways of the Force until his death at the age of 900 in Return of the Jedi, though he later returns as a Force spirit. In the prequel trilogy, Yoda leads the Jedi High Council and trains the young generations of Jedi until they are assigned to a master. When the Clone Wars breaks out, he becomes a general in the Galactic Republic's army and leads several legions of clone troopers. Yoda is one of the few Jedi to survive the events of Order 66 at the end of the war, and faces off against Emperor Palpatine, but is outmatched and forced to go into hiding. Yoda's spirit appears again in the sequel trilogy, advising an older Luke and empowering Rey to face a revived Palpatine.
   </p>
 
 </section>
