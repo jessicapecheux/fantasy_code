c6a4a41028a2bd539ff067a9f2a693b1b62b569f
J007B <c.fougeroux007@gmail.com>
2022-03-25 11:52:27 +0100

    add title pop

--- _footer.php
+++ _footer.php
@@ -2,19 +2,19 @@
         <h2 id= "contact_us"><a href="contact.php">Contact us</a></h2>
         <div class= "icon_medias">
                 <a href="https://fr-fr.facebook.com/">
-                        <img src="assets/images/facebook.png" alt="facebook"  />
+                        <img src="assets/images/facebook.png" alt="facebook" title= "facebook" />
                 </a>
                 <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZnIifQ%3D%3D%22%7D">
-                        <img src="assets/images/twitter.png" alt="twitter"  />
+                        <img src="assets/images/twitter.png" alt="twitter" title="twitter" />
                 </a>
                 <a href="https://www.instagram.com/?hl=fr">
-                        <img src="assets/images/instagram.png" alt="instagram"  />
+                        <img src="assets/images/instagram.png" alt="instagram" title="instagram" />
                 </a>
                 <a href="mailto:https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
-                        <img src="assets/images/gmail.png" alt="gmail"  />
+                        <img src="assets/images/gmail.png" alt="gmail" title="link to mail direction" />
                 </a>
                 <a class="phone" href="tel:=+33000000007">
-                        <img src="assets/images/phone.png" alt="phone"  />
+                        <img src="assets/images/phone.png" alt="phone" title="call us" />
                 </a>
                 <h3 class= "tel_number">0.000.000.007</h3>
         </div>
