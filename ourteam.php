da0909b7bcf019184bc3bf511e06f512838c40a5
Yoann Brambilla <yobrambilla@gmail.com>
2022-03-25 09:27:03 +0100

    Morning commit

--- ourteam.php
+++ ourteam.php
@@ -18,7 +18,7 @@
 
       <h3>Front End Dev</h3>
       <div class="hulk">
-        <img src="/assets/images/hulk.png" alt="Hulk" width="200px" class="hulkPic" />
+        <img src="/assets/images/hulk.png" alt="Hulk" class="hulkPic" />
         <p class="hulkQuote">
           <i>
             " Cool but angry,<br> really ANGRY "
@@ -37,7 +37,7 @@
       <h2>"Homer Simpson"</h2>
       <h3>Data Analyst</h3>
       <div class="homer">
-        <img src="/assets/images/homer.png" alt="Homer" width="200px" class="homerPic" />
+        <img src="/assets/images/homer.png" alt="Homer" class="homerPic" />
         <p class="homerQuote">
           <i>
             " I Am So Smart!<br>so S.M.R.T ! d'oh "</i>
@@ -55,7 +55,7 @@
       <h2><strong>" Batman </strong><i> Aka </i> Bruce Wayne "</h2>
       <h3>Back End Dev</h3>
       <div class="batman">
-        <img src="/assets/images/batman.png" alt="Batman" width="250px" class=" batmanPic" />
+        <img src="/assets/images/batman.png" alt="Batman" class=" batmanPic" />
         <p class="batmanQuote">
           <i>
             " You really want <br> became a Joker "</i>
@@ -70,7 +70,7 @@
       <h2>" Master Yoda "</h2>
       <h3>Web Designer</h3>
       <div class="yoda">
-        <img src="/assets/images/yoda.png" alt="Yoda" width="250px" class="yodaPic" />
+        <img src="/assets/images/yoda.png" alt="Yoda" class="yodaPic" />
         <p class="yodaQuote">
           <i>
             " Do or do not.<br> There is no try "</i>
